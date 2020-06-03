48fce4d66da7651c7695998ae02204dca47c087a
gierMann <thisisgie.21@gmail.com>
2020-06-03 21:32:15 +0700

    removed duplicate in random logic

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -22,15 +22,44 @@
 
     Animation.prototype.playButtonClick = function(){
 
+        function inArray(arr, el) {
+            for(var i = 0 ; i < arr.length; i++) 
+                    if(arr[i] == el) return true;
+            return false;
+        }
+        
+        function getRandomIntNoDuplicates(min, max, DuplicateArr) {
+            var RandomInt = Math.floor(Math.random() * (max - min + 1)) + min;
+            if (DuplicateArr.length > (max-min) ) return false;  // break endless recursion
+            if(!inArray(DuplicateArr, RandomInt)) {
+               DuplicateArr.push(RandomInt); 
+               return RandomInt;
+            }
+            return getRandomIntNoDuplicates(min, max, DuplicateArr); //recurse
+        }      
+        
+        var duplicates  =[];
+       
+
         $(".btn-play").on("click", function(){
-            var item = Math.floor((Math.random() * 22) + 0);
+            // var item = Math.floor((Math.random() * 22) + 0);
+            console.log(duplicates)
+            for (var i = 1; i <= 1 ; i++) { 
+                var item = getRandomIntNoDuplicates(0,22,duplicates);
+                console.log(item);
+            } 
 
+            if(item !== false){
+                $(this).parent().siblings().find("source").attr("src", "https://www.mboxdrive.com/love-"+item+".mp3")
+                setTimeout(function(){
+                    $("#audio")[0].load();
+                    $("#audio")[0].play();
+                },300)
+            }else {
+                
+                window.location.reload()
+            }
 
-            $(this).parent().siblings().find("source").attr("src", "https://www.mboxdrive.com/love-"+item+".mp3")
-            setTimeout(function(){
-                $("#audio")[0].load();
-                $("#audio")[0].play();
-            },300)
 
 
             
